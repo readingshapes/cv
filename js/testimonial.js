9f02b6980369e6cea07479393df83d130e067bdb
Kari <kari@readingshapes.com>
2022-05-02 00:55:06 -0700

    redid bump chart
    changed p ids to class, removed netlify-related uneeded files. Now with temp new svg bump chart files.

---
+++ js/testimonial.js
@@ -0,0 +1,19 @@
+// make the testimonial carousel go
+$(Intro Page).ready(function(){
+  $(".testimonial .indicators li").click(function(){
+    var i = $(this).index();
+    var targetElement = $(".testimonial .tabs li");
+    targetElement.eq(i).addClass('active');
+    targetElement.not(targetElement[i]).removeClass('active');
+            });
+            $(".testimonial .tabs li").click(function(){
+                var targetElement = $(".testimonial .tabs li");
+                targetElement.addClass('active');
+                targetElement.not($(this)).removeClass('active');
+            });
+        });
+$(Intro Page).ready(function(){
+    $(".slider .swiper-pagination span").each(function(i){
+        $(this).text(i+1).prepend("0");
+    });
+});
